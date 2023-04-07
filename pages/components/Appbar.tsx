e804309c09f35c6a25d1a766fcbd3aa514f9e353
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-07 03:18:06 -0400

    created a responsive appbar

---
+++ pages/components/Appbar.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { HiMenu, HiX } from 'react-icons/hi'
+
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'About', href: '#' },
+  { name: 'Microgreens', href: '#' },
+  { name: "FAQ's", href: '#' },
+  { name: 'Contact', href: '#' },
+]
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="shadow shadow-primary-900/10 bg-primary-50 dark:bg-primary-950">
+      <nav className="flex items-center justify-between p-4 mx-auto max-w-7xl lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-primary-700 dark:text-primary-200"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <HiMenu className="w-6 h-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-primary-900 dark:text-primary-200">
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-primary-100 dark:bg-primary-950 sm:max-w-sm sm:ring-1 sm:ring-primary-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="w-auto h-8"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-primary-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <HiX className="w-6 h-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="flow-root mt-6">
+            <div className="-my-6 divide-y divide-primary-500/10">
+              <div className="py-6 space-y-2">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 rounded-lg text-primary-900 dark:text-primary-100 hover:bg-primary-50 hover:dark:bg-primary-900"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
