79c73afdee399e496c48014530cf6f8eb5bc3bbd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-07 03:16:19 -0400

    set "Public Sans" as the default sans-serif font, and included custom brand colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: "#f2faeb",
+          100: "#e4f2d5",
+          200: "#cae7af",
+          300: "#a7d680",
+          400: "#86c356",
+          500: "#69a939",
+          600: "#4f862a",
+          700: "#3e6724",
+          800: "#345222",
+          900: "#2e4720",
+          950: "#15260d",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
