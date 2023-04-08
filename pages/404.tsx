0b6ea13aae208850ff7f851f938c3d6695f795b1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-07 21:34:43 -0400

    created an error page

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Error404() {
+  return (
+    <main className="w-full h-[90vh] flex place-content-center place-items-center">
+      <div className="text-center">
+        <p className="text-base font-semibold text-primary-600">404</p>
+        <h1 className="mt-2 font-bold capitalize display-medium">Page not found.</h1>
+        <p className="mt-2 body-large">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div className="mt-6">
+          <Link href="/" className="text-base font-medium text-primary-600 hover:text-primary-500">
+            Go back home
+            <span aria-hidden="true"> &rarr;</span>
+          </Link>
+        </div>
+      </div>
+    </main>
+  )
+}
