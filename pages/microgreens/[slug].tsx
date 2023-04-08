b34605c4b6ace9737c28164f77801745d61d91a1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-07 21:35:05 -0400

    created a sample page

---
+++ pages/microgreens/[slug].tsx
@@ -0,0 +1,12 @@
+import Appbar from '@/components/Appbar'
+import Footer from '@/components/Footer'
+
+export default function MicrogreensSlug() {
+  return (
+    <>
+    <Appbar />
+      <main className="wrapper-sapce wrapper ">[slug]</main>
+    <Footer />
+    </>
+  )
+}
