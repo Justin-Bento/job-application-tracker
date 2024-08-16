a55eaf4985b1d62b2912c00b5e084cd77c7585f6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-16 11:20:23 -0400

    Moved hero section back to main function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
-import Appbar from "../components/Appbar"
-import Footer from "../components/Footer"
-import Head from "next/head"
-import { incentives, FAQs } from "@/lib/data"
-import Link from "next/link"
+import Image from "next/image";
+import Appbar from "../components/Appbar";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import { incentives, FAQs } from "@/lib/data";
+import Link from "next/link";
 import Logo from "@/components/Logo";
 
 export default function index() {
@@ -12,11 +12,60 @@ export default function index() {
       <Head>
         <title>Urban Greens Antigua</title>
         <meta name="title" content="Urban Greens Antigua" />
-        <meta name="description" content="Discover the unbeatable flavor and nutritional benefits of incorporating sustainable microgreens into your lifestyle in Antigua & Barbuda" />
+        <meta
+          name="description"
+          content="Discover the unbeatable flavor and nutritional benefits of incorporating sustainable microgreens into your lifestyle in Antigua & Barbuda"
+        />
       </Head>
       <Appbar />
       <main className="bg-white dark:bg-black shadow-sm border-b">
-        <HeroSection />
+        <section className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="py-24 space-y-6">
+            <div className="justify-start hidden sm:flex sm:justify-center">
+              <div className="relative px-3 py-1 text-sm leading-6 rounded-full text-primary dark:text-primary-50 ring-1 ring-black/20">
+                Anim aute id magna aliqua ad ad non deserunt sunt.
+                <a href="#" className="font-semibold ml-2">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <h1 className="text-5xl dark:text-white font-bold text-center">
+              Sustainable Microgreens, Unbeatable Flavor!
+            </h1>
+            <p className="text-balance text-lg leading-6 tracking-wide max-w-[75ch] mx-auto text-center">
+              Our microgreens are packed with nutrients and flavor, making them the perfect addition to any meal. From
+              salads to smoothies, sandwiches to sushi, the possibilities are endless. Plus, with our convenient
+              delivery service, you can easily incorporate microgreens into your diet without leaving your home.
+            </p>
+            <div className="flex flex-col justify-start gap-4 md:flex-row md:justify-center">
+              <Link
+                href="/microgreens"
+                className="px-3 py-2 text-sm font-semibold rounded-md text-white bg-primary hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              >
+                Preview Our Greens
+              </Link>
+              <Link
+                href="/about"
+                className="px-3 py-2 text-sm font-semibold bg-transparent rounded-md text-primary ring-1 ring-inset ring-primary hover:bg-primary/5"
+              >
+                About Urban Greens
+              </Link>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-transparent ">
+            <div className="px-4 py-5 sm:p-6 w-full h-[420px] md:h-[640px] relative z-10">
+              <Image
+                fill
+                quality={40}
+                src="/media/urban-greens-antigua-hero.jpg"
+                alt="Hodges Bay Club garnished with our Island Microgreens"
+                className="object-cover object-center rounded-xl"
+              />
+            </div>
+          </div>
+        </section>
+        {/* Section #1 - Hero */}
         <Incentives />
         <CallToAction_Products />
         <Product_Benifites />
@@ -25,48 +74,7 @@ export default function index() {
       </main>
       <Footer />
     </>
-  )
-}
-
-function HeroSection() {
-  return (
-    <>
-      <section className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div className="py-24 space-y-6">
-          <div className="justify-start hidden sm:flex sm:justify-center">
-            <div className="relative px-3 py-1 text-sm leading-6 rounded-full text-primary dark:text-primary-50 ring-1 ring-black/20">
-              Anim aute id magna aliqua ad ad non deserunt sunt.
-              <a href="#" className="font-semibold ml-2">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <h1 className="text-5xl dark:text-white font-bold text-center">Sustainable Microgreens, Unbeatable Flavor!</h1>
-          <p className="text-balance text-lg leading-6 tracking-wide max-w-[75ch] mx-auto text-center">Our microgreens are packed with nutrients and flavor, making them the perfect addition to any meal. From salads to smoothies, sandwiches to sushi, the possibilities are endless. Plus, with our convenient delivery service, you can easily incorporate microgreens into your diet without leaving your home.</p>
-          <div className="flex flex-col justify-start gap-4 md:flex-row md:justify-center">
-            <Link
-              href="/microgreens"
-              className="px-3 py-2 text-sm font-semibold rounded-md text-white bg-primary hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-            >
-              Preview Our Greens
-            </Link>
-            <Link
-              href="/about"
-              className="px-3 py-2 text-sm font-semibold bg-transparent rounded-md text-primary ring-1 ring-inset ring-primary hover:bg-primary/5"
-            >
-              About Urban Greens
-            </Link>
-          </div>
-        </div>
-        <div className="overflow-hidden bg-transparent ">
-          <div className="px-4 py-5 sm:p-6 w-full h-[420px] md:h-[640px] relative z-10">
-            <Image fill quality={40} src="/media/urban-greens-antigua-hero.jpg" alt="Hodges Bay Club garnished with our Island Microgreens" className="object-cover object-center rounded-xl" />
-          </div>
-        </div>
-      </section>
-    </>
-  )
+  );
 }
 
 function Incentives() {
@@ -92,7 +100,7 @@ function Incentives() {
         </div>
       </section>
     </>
-  )
+  );
 }
 function Product_Benifites() {
   return (
@@ -102,7 +110,8 @@ function Product_Benifites() {
           <div>
             <h3 className="mb-4 font-bold capitalize headline-large">Clear overview for efficient tracking</h3>
             <p className="mb-5 body-large">
-              Handle your subscriptions and transactions efficiently with the clear overview in Dashboard. Features like the smart search option allow you to quickly find any data you’re looking for.
+              Handle your subscriptions and transactions efficiently with the clear overview in Dashboard. Features like
+              the smart search option allow you to quickly find any data you’re looking for.
             </p>
             <button
               type="button"
@@ -112,15 +121,22 @@ function Product_Benifites() {
             </button>
           </div>
           <div className="w-full h-[420px] relative bg-primary-200 dark:bg-primary-900 rounded-xl">
-            <Image fill quality={40} src="/media/urban-greens-auntigua.jpg" alt="" aria-label="micro-greens-inside-steak sandwitch" className="object-cover object-center rounded-xl" />
+            <Image
+              fill
+              quality={40}
+              src="/media/urban-greens-auntigua.jpg"
+              alt=""
+              aria-label="micro-greens-inside-steak sandwitch"
+              className="object-cover object-center rounded-xl"
+            />
           </div>
         </div>
         <div className="grid flex-col-reverse items-center grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
           <div className="order-none md:order-2">
             <h3 className="mb-4 font-bold capitalize headline-large">Decide how you integrate Payments</h3>
             <p className="mb-5 body-large">
-              Love to code? Next to our ready-made and free plugins you can use our expansive yet simple API; decide how you integrate Payments and build advanced and reliable products yourself from
-              scratch.
+              Love to code? Next to our ready-made and free plugins you can use our expansive yet simple API; decide how
+              you integrate Payments and build advanced and reliable products yourself from scratch.
             </p>
             <button
               type="button"
@@ -130,20 +146,30 @@ function Product_Benifites() {
             </button>
           </div>
           <div className="w-full h-[420px] relative bg-primary-200 dark:bg-primary-900 rounded-xl">
-            <Image fill quality={40} src="/media/micro-greens-with-steak.jpg" alt="" aria-label="micro-greens-inside-steak sandwitch" className="object-cover object-center rounded-xl" />
+            <Image
+              fill
+              quality={40}
+              src="/media/micro-greens-with-steak.jpg"
+              alt=""
+              aria-label="micro-greens-inside-steak sandwitch"
+              className="object-cover object-center rounded-xl"
+            />
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 function Testimonials() {
   return (
     <>
-      <section className="max-w-7xl mx-auto relative px-6 py-24 overflow-hidden text-left isolate sm:rounded-3xl sm:px-16 rounded-xl  bg-secondary" aria-label="Section 05">
+      <section
+        className="max-w-7xl mx-auto relative px-6 py-24 overflow-hidden text-left isolate sm:rounded-3xl sm:px-16 rounded-xl  bg-secondary"
+        aria-label="Section 05"
+      >
         <div className="relative max-w-2xl mx-auto lg:mx-0">
           <div className="relative w-auto h-12">
-          <Logo classNames="w-20 h-20 fill-gray-900 hover:opacity-80 transition-all" />
+            <Logo classNames="w-20 h-20 fill-gray-900 hover:opacity-80 transition-all" />
           </div>
           <figure>
             <blockquote className="mt-6 font-bold title-large">
@@ -160,15 +186,20 @@ function Testimonials() {
         </div>
       </section>
     </>
-  )
+  );
 }
 function CallToAction_Products() {
   return (
     <>
-      <section className="max-w-7xl mx-auto lg:px-8 relative px-6 py-24 overflow-hidden text-center isolate sm:rounded-3xl sm:px-16 bg-secondary" aria-label="Section 05">
+      <section
+        className="max-w-7xl mx-auto lg:px-8 relative px-6 py-24 overflow-hidden text-center isolate sm:rounded-3xl sm:px-16 bg-secondary"
+        aria-label="Section 05"
+      >
         <h5 className="font-bold headline-small">Make Microgreens Part Of Your Diet Today!</h5>
-        <p className="mt-4 text-lg body-large lg:max-w-prose lg:mx-auto ">Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
-          commodo do ea.</p>
+        <p className="mt-4 text-lg body-large lg:max-w-prose lg:mx-auto ">
+          Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
+          commodo do ea.
+        </p>
         <div className="flex items-center justify-center mt-10 gap-x-6">
           <a
             href="#"
@@ -176,13 +207,16 @@ function CallToAction_Products() {
           >
             Order Today
           </a>
-          <a href="#" className="px-3 py-2 text-sm font-semibold bg-transparent rounded-md text-primary-600 ring-1 ring-inset ring-black hover:bg-black/5">
+          <a
+            href="#"
+            className="px-3 py-2 text-sm font-semibold bg-transparent rounded-md text-primary-600 ring-1 ring-inset ring-black hover:bg-black/5"
+          >
             Learn more <span aria-hidden="true">→</span>
           </a>
         </div>
       </section>
     </>
-  )
+  );
 }
 
 function CallToAction_SocialMedia() {
@@ -190,10 +224,15 @@ function CallToAction_SocialMedia() {
     <>
       <section className="max-w-7xl mx-auto sm:px-6 lg:px-8" aria-label="Section 06">
         <div className="px-6 py-16 mx-auto max-w-7xl sm:py-24 lg:px-8">
-          <h2 className="mt-2 text-3xl font-bold tracking-tight capitalize text-primary-900 sm:text-4xl">Frequently asked questions</h2>
+          <h2 className="mt-2 text-3xl font-bold tracking-tight capitalize text-primary-900 sm:text-4xl">
+            Frequently asked questions
+          </h2>
           <p className="max-w-3xl mt-6 text-lg leading-8 text-primary-800">
             Have a different question and can’t find the answer you’re looking for? Reach out to our support team by
-            <Link href="mailto:info@urbangreensanu.com" className="font-semibold text-primary-600 hover:text-primary-500">
+            <Link
+              href="mailto:info@urbangreensanu.com"
+              className="font-semibold text-primary-600 hover:text-primary-500"
+            >
               sending us an email
             </Link>
             and we’ll get back to you as soon as we can.
@@ -211,5 +250,5 @@ function CallToAction_SocialMedia() {
         </div>
       </section>
     </>
-  )
+  );
 }
