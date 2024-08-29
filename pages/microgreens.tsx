b49098187230306034bf3254fa3734c927dfde39
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-29 16:23:55 -0400

    Mapped the microgreens_plants based off the data

--- pages/microgreens.tsx
+++ pages/microgreens.tsx
@@ -27,6 +27,18 @@ export default function microgreens() {
           </p>
         </section>
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {microgreens_plants.map((plants, index) => {
+            return (
+              <>
+                <Card
+                  Headline={plants.title}
+                  title={plants.title}
+                  link={`/microgreens/${plants.slug}`}
+                  description={plants.description}
+                />
+              </>
+            );
+          })}
         </section>
       </main>
       <Footer />
