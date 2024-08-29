4f402d48ffa30ea8b55737fa50471ea54b9b8acb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-28 22:35:42 -0400

    removed custom classNames for the container and background.
    * Added custom tailwind color
    * Replaced wrapper with container.

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,36 +8,41 @@ export default function about() {
       <Head>
         <title>About - Urban Greens Antigua</title>
         <meta name="title" content="Urban Greens Antigua" />
-        <meta name="description" content="Discover the unbeatable flavor and nutritional benefits of incorporating sustainable microgreens into your lifestyle in Antigua & Barbuda" />
+        <meta
+          name="description"
+          content="Discover the unbeatable flavor and nutritional benefits of incorporating sustainable microgreens into your lifestyle in Antigua & Barbuda"
+        />
       </Head>
       <Appbar />
-      <main className="wrapper-sapce wrapper">
-        <section className="">
-          <h1 className="headline-large">Headline 01</h1>
-        </section>
-        {/* End Of Section 01  */}
-        <section className="">
-          <h2 className="headline-large">Headline 02</h2>
-        </section>
-        {/* End Of Section 01  */}
-        <section className="">
-          <h3 className="headline-large">Headline 03</h3>
-        </section>
-        {/* End Of Section 01  */}
-        <section className="">
-          <h4 className="headline-large">Headline 04</h4>
-        </section>
-        {/* End Of Section 01  */}
-        <section className="">
-          <h5 className="headline-large">Headline 05</h5>
-        </section>
-        {/* End Of Section 01  */}
-        <section className="">
-          <h6 className="headline-large">Headline 01</h6>
-        </section>
-        {/* End Of Section 01  */}
+      <main className="bg-white">
+        <div className="container mx-auto sm:px-6 lg:px-8 py-4">
+          <section className="">
+            <h1 className="headline-large">Headline 01</h1>
+          </section>
+          {/* End Of Section 01  */}
+          <section className="">
+            <h2 className="headline-large">Headline 02</h2>
+          </section>
+          {/* End Of Section 01  */}
+          <section className="">
+            <h3 className="headline-large">Headline 03</h3>
+          </section>
+          {/* End Of Section 01  */}
+          <section className="">
+            <h4 className="headline-large">Headline 04</h4>
+          </section>
+          {/* End Of Section 01  */}
+          <section className="">
+            <h5 className="headline-large">Headline 05</h5>
+          </section>
+          {/* End Of Section 01  */}
+          <section className="">
+            <h6 className="headline-large">Headline 01</h6>
+          </section>
+          {/* End Of Section 01  */}
+        </div>
       </main>
       <Footer />
     </>
-  )
+  );
 }
