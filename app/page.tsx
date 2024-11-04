87d1c03415b7e5545759896f6654e188ec4dd8ca
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-04 15:47:37 -0500

    Added styles to have a a dynamic viewport height of 100 percent of the screen and a wrapper class

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <main className="">
+      <main className="min-h-dvh container mx-auto">
         <h1 className="">Welcome, Home page!</h1>
       </main>
     </>
