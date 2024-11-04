c3ed3e1f6b5b3cad4c5806aa33ecd385d91bf82d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-04 15:55:31 -0500

    Applied global wrrapper to header and footer elements

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="p-6 bg-gray-200">
+    <footer className="p-6 bg-gray-200 container mx-auto">
       <p className="">footer</p>
     </footer>
   );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="shadow p-6">
+    <header className="shadow p-6 container mx-auto">
       <p className="">Header</p>
     </header>
   );
