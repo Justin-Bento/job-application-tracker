83ef5275df674b2b0c40b7435633f6aeb6ebe1f5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-07 14:24:14 -0500

    Added a paragraph to describe the purpose of the page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ export default function page() {
       <main className="min-h-dvh">
         <section className="container mx-auto p-6">
           <h1 className="text-4xl font-bold tracking-[-0.05rem] capitalize">Welcome, Home page!</h1>
+          <p className="text-base/6 max-w-[100ch] my-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime soluta exercitationem, ipsum enim ab
+            quibusdam perspiciatis facilis reiciendis veniam nisi, ipsa iusto, deserunt explicabo natus aliquam dolores?
+            Non, quod natus.
+          </p>
           <Divider className="w-24 h-1" />
         </section>
       </main>
