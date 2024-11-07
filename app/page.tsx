8309d7c95639e1fd646c571a70b4d9ffc2ab26fd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-07 14:28:22 -0500

    Created an array that loops the content 10 times and adjusted spacing between the content section of the website

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function page() {
   return (
     <>
       <Header />
-      <main className="min-h-dvh">
-        <section className="container mx-auto px-6 pt-24">
+      <main className="min-h-dvh space-y-6 py-24">
+        <section className="container mx-auto px-6">
           <h1 className="text-4xl font-bold tracking-[-0.05rem] capitalize">Welcome, Home page!</h1>
           <p className="text-base/6 max-w-[100ch] my-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime soluta exercitationem, ipsum enim ab
@@ -18,14 +18,18 @@ export default function page() {
           <Divider className="w-24 h-1" />
         </section>
         <section className="container mx-auto px-6">
-          <div className="overflow-hidden rounded-lg border border-gray-200 mt-6">
-            <div className="px-4 py-5 sm:p-6 space-y-2">
-              <h2 className="font-semibold capitalize">Title of this card goes here.</h2>
-              <p className="">
-                This card links to other routes within the application and that is my quick summary of this card.
-              </p>
-            </div>
-          </div>
+          {Array.from({ length: 10 }, (_, index) => {
+            return (
+              <div key={index} className="overflow-hidden rounded-lg border border-gray-200 mt-6">
+                <div className="px-4 py-5 sm:p-6 space-y-2">
+                  <h2 className="font-semibold capitalize">Title of this card goes here.</h2>
+                  <p className="">
+                    This card links to other routes within the application and that is my quick summary of this card.
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </section>
       </main>
       <Footer />
