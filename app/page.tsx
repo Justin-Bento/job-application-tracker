03393b138792b9798f22dee169e8d69be5c831ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-07 14:25:21 -0500

    Created a card that stores the content

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
     <>
       <Header />
       <main className="min-h-dvh">
-        <section className="container mx-auto p-6">
+        <section className="container mx-auto px-6 pt-24">
           <h1 className="text-4xl font-bold tracking-[-0.05rem] capitalize">Welcome, Home page!</h1>
           <p className="text-base/6 max-w-[100ch] my-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime soluta exercitationem, ipsum enim ab
@@ -16,6 +16,16 @@ export default function page() {
             Non, quod natus.
           </p>
           <Divider className="w-24 h-1" />
+        </section>
+        <section className="container mx-auto px-6">
+          <div className="overflow-hidden rounded-lg border border-gray-200 mt-6">
+            <div className="px-4 py-5 sm:p-6 space-y-2">
+              <h2 className="font-semibold capitalize">Title of this card goes here.</h2>
+              <p className="">
+                This card links to other routes within the application and that is my quick summary of this card.
+              </p>
+            </div>
+          </div>
         </section>
       </main>
       <Footer />
