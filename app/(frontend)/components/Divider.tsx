55a7c2351ea0805e0d2253a45d4cf4031598f947
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-12 10:11:36 -0400

    Deleted Uncessary files

--- app/(frontend)/components/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface DividerProps {
-  className?: string;
-}
-
-export default function Divider({ className = "" }: DividerProps) {
-  return <div className={`h-1 rounded-full bg-black mt-4 ${className}`}></div>;
-}
