805be516e64d9b2d6d5944043cc8910d1d36a7bb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-20 02:10:56 -0500

    Added a react fragment to the position

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,24 +5,26 @@ import { POSTS_QUERY } from "@/sanity/lib/quires";
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
   return (
-    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Home</h1>
-      <ul className="grid grid-cols-1 divide-y divide-gray-300">
-        {posts.map((post) => {
-          return (
-            <li key={post._id}>
-              <Link
-                className="block p-4 hover:text-blue-500"
-                href={`/posts/${post?.slug?.current}`}
-              >
-                {post?.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <hr />
-      <Link href="/posts">Posts index &rarr;</Link>
-    </section>
+    <>
+      <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
+        <h1 className="text-4xl font-bold">Digital Recipie Book</h1>
+        <ul className="grid grid-cols-1 divide-y divide-gray-300">
+          {posts.map((post) => {
+            return (
+              <li key={post._id}>
+                <Link
+                  className="block p-4 hover:text-blue-500"
+                  href={`/posts/${post?.slug?.current}`}
+                >
+                  {post?.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <hr />
+        <Link href="/posts">Posts index &rarr;</Link>
+      </section>
+    </>
   );
 }
