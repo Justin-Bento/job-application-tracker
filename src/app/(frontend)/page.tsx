9888fb1960aac959595c72b8261dcb5ddc3293b3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-20 02:17:58 -0500

    moved the recipie book into it's only section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,23 +4,24 @@ import { POSTS_QUERY } from "@/sanity/lib/quires";
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
+  const recipie_book = posts.map((post) => {
+    return (
+      <li key={post._id}>
+        <Link
+          className="block p-4 hover:text-blue-500"
+          href={`/posts/${post?.slug?.current}`}
+        >
+          {post?.title}
+        </Link>
+      </li>
+    );
+  });
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12 space-y-16">
       <section className="space-y-4">
         <h1 className="text-4xl font-bold">Digital Recipie Book</h1>
         <ul className="grid grid-cols-1 divide-y divide-gray-300">
-          {posts.map((post) => {
-            return (
-              <li key={post._id}>
-                <Link
-                  className="block p-4 hover:text-blue-500"
-                  href={`/posts/${post?.slug?.current}`}
-                >
-                  {post?.title}
-                </Link>
-              </li>
-            );
-          })}
+          {recipie_book}
         </ul>
         <hr />
       </section>
