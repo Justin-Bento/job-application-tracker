0194aa4393aaca574f2c8809cc2322b1d7a45820
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-16 01:43:49 -0400

    Added a link to the main page

--- src/components/NavigationTop.tsx
+++ src/components/NavigationTop.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 
 export default function NavigationTop() {
@@ -7,14 +8,16 @@ export default function NavigationTop() {
     <footer>
       <Card className="rounded-none border-0">
         <CardContent className="w-screen container mx-auto px-0">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={38}
-            priority
-          />
+          <Link href="/" className="hover:opacity-50">
+            <Image
+              className="dark:invert"
+              src="/next.svg"
+              alt="Next.js logo"
+              width={100}
+              height={38}
+              priority
+            />
+          </Link>
         </CardContent>
       </Card>
     </footer>
