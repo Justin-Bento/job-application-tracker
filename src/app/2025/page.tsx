5e70d13f41902188498b2b469bc8cf9b9889d2cd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-16 11:13:07 -0400

    Restored page back to default

--- src/app/2025/page.tsx
+++ src/app/2025/page.tsx
@@ -1,80 +1,70 @@
-// import Link from "next/link";
-// import React from "react";
-// import { ExternalLink } from "lucide-react";
-// import { Card, CardContent } from "@/components/ui/card";
-
-// interface JobPost {
-//   post: {
-//     index: number;
-//     Title: string;
-//     Company: string;
-//     Location: string;
-//     Response: string;
-//     Updates: string;
-//     Offers: string;
-//     jobURL: string;
-//   };
-//   index: number;
-// }
-// export default async function Page() {
-//   const data = await fetch("http://localhost:3000/api");
-//   const posts = await data.json();
-//   return (
-//     // <main className="container mx-auto my-16">
-//     //   <section className="space-y-2">
-//     //     <h1 className="text-4xl font-semibold leading-snug capitalize">
-//     //       Jobs I&#39;ve Applied to in 2025.
-//     //     </h1>
-//     //     <p className="max-w-4xl text-xl text-balance">
-//     //       Sent out {posts.length} applications.
-//     //     </p>
-//     //   </section>
-//     //   <section className="mt-6">
-//     //     <ul className="space-y-6 grid grid-cols-1 xl:grid-cols-2 gap-6">
-//     //       {posts.map(({ post, index }: JobPost) => (
-//     //         <li key={index}>
-//     //           <Card className="shadow-none">
-//     //             <CardContent className="space-y-4">
-//     //               <p className="text-xs text-muted-foreground">
-//     //                 Application #{index + 1}
-//     //               </p>
-//     //               <p className="text-lg">
-//     //                 <strong>{post.Title}</strong>,&nbsp;
-//     //                 <span className="capitalize">{post.Company}</span>
-//     //               </p>
-
-//     //               <ul className="flex items-center gap-4 divide-x-2 [&>li]:pr-4 [&>li]:text-sm [&>li]:text-muted-foreground [&>li]:capitalize">
-//     //                 <li className="">{post.Location}</li>
-//     //                 <li className="">{post.Response}</li>
-//     //                 <li className="">{post.Updates}</li>
-//     //                 <li className="">{post.Offers || "No Offers"}</li>
-//     //                 {post.jobURL == "" ? (
-//     //                   <li>No Link To Post</li>
-//     //                 ) : (
-//     //                   <li className="flex items-center gap-1 hover:underline">
-//     //                     <ExternalLink className="size-3.5" />
-//     //                     <Link href={post.jobURL}>Link To Post</Link>
-//     //                   </li>
-//     //                 )}
-//     //               </ul>
-//     //             </CardContent>
-//     //           </Card>
-//     //         </li>
-//     //       ))}
-//     //     </ul>
-//     //   </section>
-//     // </main>
-//   );
-// }
-
+import Link from "next/link";
 import React from "react";
+import { ExternalLink } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
 
-export default function page() {
+interface JobPost {
+  post: {
+    index: number;
+    Title: string;
+    Company: string;
+    Location: string;
+    Response: string;
+    Updates: string;
+    Offers: string;
+    jobURL: string;
+  };
+  index: number;
+}
+export default async function Page() {
+  const data = await fetch(
+    "https://job-application-tracker-smoky-sigma.vercel.app/api"
+  );
+  const posts = await data.json();
   return (
-    <main className="">
-      <h1 className="text-4xl font-semibold leading-snug capitalize">
-        Hello, Next.js page
-      </h1>
+    <main className="container mx-auto my-16">
+      <section className="space-y-2">
+        <h1 className="text-4xl font-semibold leading-snug capitalize">
+          Jobs I&#39;ve Applied to in 2025.
+        </h1>
+        <p className="max-w-4xl text-xl text-balance">
+          Sent out {posts.length} applications.
+        </p>
+      </section>
+      <section className="mt-6">
+        <ul className="space-y-6 grid grid-cols-1 xl:grid-cols-2 gap-6">
+          {posts.map(({ post, index }: JobPost) => (
+            <li key={index}>
+              <Card className="shadow-none">
+                <CardContent className="space-y-4">
+                  <p className="text-xs text-muted-foreground">
+                    Application #{index + 1}
+                  </p>
+                  <p className="text-lg">
+                    <strong>{post.Title}</strong>,&nbsp;
+                    <span className="capitalize">{post.Company}</span>
+                  </p>
+
+                  <ul className="flex items-center gap-4 divide-x-2 [&>li]:pr-4 [&>li]:text-sm [&>li]:text-muted-foreground [&>li]:capitalize">
+                    <li className="">{post.Location}</li>
+                    <li className="">{post.Response}</li>
+                    <li className="">{post.Updates}</li>
+                    <li className="">{post.Offers || "No Offers"}</li>
+                    {post.jobURL == "" ? (
+                      <li>No Link To Post</li>
+                    ) : (
+                      <li className="flex items-center gap-1 hover:underline">
+                        <ExternalLink className="size-3.5" />
+                        <Link href={post.jobURL}>Link To Post</Link>
+                      </li>
+                    )}
+                  </ul>
+                </CardContent>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
