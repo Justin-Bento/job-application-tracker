ec628bed71a958f20619e36abcfe66134befd1cc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-18 11:33:45 -0400

    Updated compononet to include a class anem extention for custom classes

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default function Example({ children }: { children?: React.ReactNode }) {
+export default function Example(
+  className: string,
+  { children }: { children?: React.ReactNode }
+) {
   return (
-    <div className="mx-auto max-w-7xl xl:container px-4 sm:px-6 lg:px-8 my-16">
+    <div
+      className={`mx-auto max-w-7xl xl:container px-4 sm:px-6 lg:px-8 ${className}`}
+    >
       {children}
     </div>
   );
