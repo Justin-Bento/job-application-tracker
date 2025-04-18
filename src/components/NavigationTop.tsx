a6032f27f4f011b00a7d36bdf5e1b4d9a54ee55e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-18 11:36:25 -0400

    Updated component

--- src/components/NavigationTop.tsx
+++ src/components/NavigationTop.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Wrapper from "@/components/Wrapper";
 import { Card, CardContent } from "./ui/card";
 
 export default function NavigationTop() {
@@ -8,16 +9,18 @@ export default function NavigationTop() {
     <header className="sticky top-0">
       <Card className="rounded-none border-0 backdrop-blur-md bg-white/30">
         <CardContent className="container mx-auto px-0">
-          <Link href="/" className="hover:opacity-50">
-            <Image
-              className="dark:invert"
-              src="/next.svg"
-              alt="Next.js logo"
-              width={100}
-              height={38}
-              priority
-            />
-          </Link>
+          <Wrapper className="pys-2">
+            <Link href="/" className="hover:opacity-50">
+              <Image
+                className="dark:invert"
+                src="/next.svg"
+                alt="Next.js logo"
+                width={100}
+                height={38}
+                priority
+              />
+            </Link>
+          </Wrapper>
         </CardContent>
       </Card>
     </header>
