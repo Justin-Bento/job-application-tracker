b68de23c7593385e905483716e4365995397ac87
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-21 12:08:41 -0400

    Added interview rate to the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,6 +85,33 @@ export default function page() {
               </div>
             </CardFooter>
           </Card>
+          <Card className="@container/card">
+            <CardHeader className="relative">
+              <CardDescription>Interview Rate</CardDescription>
+              <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+                #002
+              </CardTitle>
+              <div className="absolute right-4 top-4">
+                <Badge
+                  variant="outline"
+                  className="flex gap-1 rounded-lg text-xs"
+                >
+                  <TrendingUpIcon className="size-3" />
+                  +12.5%
+                </Badge>
+              </div>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Resume improvements aided
+                <TrendingUpIcon className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Engagement exceed targets from last year but it's still below
+                avarage.
+              </div>
+            </CardFooter>
+          </Card>
         </div>
       </section>
     </Wrapper>
