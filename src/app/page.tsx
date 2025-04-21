1adbbde0ad2cfe3b9070cc34276d7efd17442d5b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-21 12:10:53 -0400

    Added response rate from the employer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,6 +112,31 @@ export default function page() {
               </div>
             </CardFooter>
           </Card>
+          <Card className="@container/card">
+            <CardHeader className="relative">
+              <CardDescription>Response Rate</CardDescription>
+              <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+                23/147 applications
+              </CardTitle>
+              <div className="absolute right-4 top-4">
+                <Badge
+                  variant="outline"
+                  className="flex gap-1 rounded-lg text-xs"
+                >
+                  <TrendingUpIcon className="size-3" />
+                  +15%
+                </Badge>
+              </div>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1 text-sm">
+              <div className="line-clamp-1 flex gap-2 font-medium">
+                Steady performance <TrendingUpIcon className="size-4" />
+              </div>
+              <div className="text-muted-foreground">
+                Meets growth projections with improvements needed.
+              </div>
+            </CardFooter>
+          </Card>
         </div>
       </section>
     </Wrapper>
