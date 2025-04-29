8324457423d0c948734029df4f2b13fc7f8e3e29
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-29 14:13:37 -0400

    Created component with full constraints

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,11 +1,30 @@
-type ExampleProps = {
-  children?: React.ReactNode;
-  className?: string; // Optional className
-};
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-export default function Wrapper({ children, className = "" }: ExampleProps) {
+// Define props (extending HTMLDivElement attributes)
+type WrapperProps = React.HTMLAttributes<HTMLDivElement> &
+  VariantProps<typeof WrapperVariants>;
+
+const WrapperVariants = cva("", {
+  variants: {
+    layout: {
+      fullWidthMobile: "mx-auto max-w-7xl sm:px-6 lg:px-8",
+      constrained: "container mx-auto px-4 sm:px-6 lg:px-8",
+    },
+    height: {
+      fullHeight: "min-h-dvh",
+    },
+  },
+});
+
+export default function Wrapper({
+  children,
+  className,
+  layout,
+  height,
+}: WrapperProps) {
   return (
-    <div className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}>
+    <div className={WrapperVariants({ layout, height, className })} {...props}>
       {children}
     </div>
   );
