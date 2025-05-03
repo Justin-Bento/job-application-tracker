fd3529194ed849fa7e74f21162fd162c9d408e11
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-03 18:51:10 -0400

    Created page for 2024 applications.

---
+++ src/app/2024/page.tsx
@@ -0,0 +1,10 @@
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+
+export default function page() {
+  return (
+    <Wrapper className="my-16">
+      <h1 className="">Hello, 2024 Page</h1>
+    </Wrapper>
+  );
+}
