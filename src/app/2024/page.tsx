2346c607ee038e021615c08aa55a24b441bd8f07
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-03 18:56:48 -0400

    Applied custom content from previous page header but updated year to 2024.

--- src/app/2024/page.tsx
+++ src/app/2024/page.tsx
@@ -6,16 +6,14 @@ export default function page() {
     <Wrapper className="my-16">
       <div className="flex flex-col gap-4">
         <h1 className="order-2 mt-2 text-5xl font-semibold tracking-tight text-foreground">
-          Hello, 2024 Page
+          Job Search Metrics 2024: Application Tally.
         </h1>
         <p className="order-3 mb-8 mt-4 text-lg text-balance text-muted-foreground sm:text-xl/7">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aut
-          nobis accusamus, molestias iste ex voluptatum iure ullam voluptatibus
-          repellendus in veritatis fugiat. Facere aspernatur asperiores, sed
-          beatae excepturi consectetur.
+          Includes relevant details such as job postings, required
+          qualifications, and application methods used..
         </p>
         <p className="order-1 text-base/7 font-semibold text-teal-600">
-          Lorem ipsum dolor sit amet.
+          Applied to # of Jobs.
         </p>
       </div>
     </Wrapper>
