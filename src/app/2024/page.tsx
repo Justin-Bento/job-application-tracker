75616d7a151b9121d73053b2af274a6db1611426
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-03 18:54:31 -0400

    Changed the css order of the page to keep the h1 tag at the top of the page.

--- src/app/2024/page.tsx
+++ src/app/2024/page.tsx
@@ -5,14 +5,14 @@ export default function page() {
   return (
     <Wrapper className="my-16">
       <div className="flex flex-col gap-4">
-        <h1 className="">Hello, 2024 Page</h1>
-        <p className="">
+        <h1 className="order-2">Hello, 2024 Page</h1>
+        <p className="order-3">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aut
           nobis accusamus, molestias iste ex voluptatum iure ullam voluptatibus
           repellendus in veritatis fugiat. Facere aspernatur asperiores, sed
           beatae excepturi consectetur.
         </p>
-        <p className="">Lorem ipsum dolor sit amet.</p>
+        <p className="order-1">Lorem ipsum dolor sit amet.</p>
       </div>
     </Wrapper>
   );
