fb1406bc793de30a9837c414901e2e453539f80d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-03 18:52:42 -0400

    Added a <div> that wraps content for the page header .

--- src/app/2024/page.tsx
+++ src/app/2024/page.tsx
@@ -4,7 +4,16 @@ import React from "react";
 export default function page() {
   return (
     <Wrapper className="my-16">
-      <h1 className="">Hello, 2024 Page</h1>
+      <div className="">
+        <h1 className="">Hello, 2024 Page</h1>
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aut
+          nobis accusamus, molestias iste ex voluptatum iure ullam voluptatibus
+          repellendus in veritatis fugiat. Facere aspernatur asperiores, sed
+          beatae excepturi consectetur.
+        </p>
+        <p className="">Lorem ipsum dolor sit amet.</p>
+      </div>
     </Wrapper>
   );
 }
