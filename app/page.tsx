240ff04e225bf38d658fed856973c26744c1d42f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-09 21:34:31 -0400

    removed default css

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main>
         <Wrapper height="fullHeight">
           <h2 className="font-medium text-xl mb-4">Next steps</h2>
           {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
