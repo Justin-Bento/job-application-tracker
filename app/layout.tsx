ceca31a91dcd29a19ebd93bf2e651a8b7fd3bd8e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-09 21:58:45 -0400

    Removed lang=en as nextjs was re-rendering the attribute causing issues with hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+    <html className={geistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
