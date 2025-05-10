1251bcb9b2e5b28915ad340cb497cec0647e6537
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-09 22:17:19 -0400

    Removed additional <Wrapper /> from page element as its rendered in <Layout>

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -27,49 +27,41 @@ export default async function ProtectedPage() {
   console.log(jobApplications);
   return (
     <>
-      <Wrapper
-        width="constrained"
-        height="fullHeight"
-        spacing="large"
-        className="space-y-16"
-      >
-        <section className="mx-auto max-w-2xl lg:mx-0 space-y-4">
-          <p className="text-sm/7 capitalize font-semibold text-teal-600">
-            Get the help you need
-          </p>
-          <h2 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-            Job Applications For 2025
-          </h2>
-          <p className="text-lg/relaxed tracking-wide text-muted-foreground">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat.
-          </p>
-        </section>
-        <form>
-          <Button className="flex flex-row-reverse items-center gap-4">
-            <PlusIcon className="size-5" />
-            Create New Job
-          </Button>
-        </form>
-        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-          {jobApplications?.map((job) => (
-            <li key={job.id}>
-              <ApplicationTracker
-                applications={job.id}
-                dateApplied={job.dateApplied}
-                title={job.title}
-                company={job.company}
-                location={job.location}
-                response={job.response}
-                updates={job.updates}
-                offers={job.offers}
-                jobURL={job.jobURL}
-              />
-            </li> // Replace `title` with your field
-          ))}
-        </ul>
-      </Wrapper>
+      <section className="mx-auto max-w-2xl lg:mx-0 space-y-4">
+        <p className="text-sm/7 capitalize font-semibold text-teal-600">
+          Get the help you need
+        </p>
+        <h2 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+          Job Applications For 2025
+        </h2>
+        <p className="text-lg/relaxed tracking-wide text-muted-foreground">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+        </p>
+      </section>
+      <form>
+        <Button className="flex flex-row-reverse items-center gap-4">
+          <PlusIcon className="size-5" />
+          Create New Job
+        </Button>
+      </form>
+      <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        {jobApplications?.map((job) => (
+          <li key={job.id}>
+            <ApplicationTracker
+              applications={job.id}
+              dateApplied={job.dateApplied}
+              title={job.title}
+              company={job.company}
+              location={job.location}
+              response={job.response}
+              updates={job.updates}
+              offers={job.offers}
+              jobURL={job.jobURL}
+            />
+          </li> // Replace `title` with your field
+        ))}
+      </ul>
     </>
   );
 }
