8f34db74b1060f0c4afc4b3d66c2bf62b2cebd2d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-09 22:02:13 -0400

    Updated names of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/Footer";
-import Hero from "@/components/hero";
+import Header from "@/components/Header";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import Wrapper from "@/components/Wrapper";
@@ -8,7 +8,7 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 export default async function Home() {
   return (
     <>
-      <Hero />
+      <Header />
       <main>
         <Wrapper height="fullHeight">
           <h2 className="font-medium text-xl mb-4">Next steps</h2>
