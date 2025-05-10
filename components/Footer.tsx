a93ddb07610b94b05dd41ffe45d0b32a3383b29f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-09 21:31:05 -0400

    Created a footer to signify the end of the page

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+
+export default function Footer() {
+  return (
+    <footer className="bg-foreground">
+      <Wrapper spacing="paddingLarge">
+        <p className="text-background">Footer</p>
+      </Wrapper>
+    </footer>
+  );
+}
